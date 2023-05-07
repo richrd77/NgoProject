7f547ff9d6357618e4645d2512a463308abf6bc7
richrd77@gmail.com <richrd77@gmail.com>
2023-05-07 16:42:58 +0530

    added interface and service

---
+++ Application/Services/SignupService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models.Entity;
+using Domain.Repositories;
+using Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    class SignupService : BaseService<User>, ISignupService
+    {
+        public SignupService(IBaseRepository<User> baseRepository) : base(baseRepository)
+        {
+        }
+    }
+}
---
+++ Domain/Services/ISignupService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public interface ISignupService : IBaseService<User>
+    {
+        
+    }
+}
